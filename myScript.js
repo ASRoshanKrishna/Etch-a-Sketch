87a5d3e28628a3baee6487a99abd2db25119daf5
Roshan Krishna A.S <asroshank@gmail.com>
2024-01-24 19:27:28 +0530

    add colors button and code random colors

--- myScript.js
+++ myScript.js
@@ -1,4 +1,5 @@
 let MAX_SIZE = 16;
+let randcolor = 0;
 
 function del(){
     let child = contain1.lastElementChild;
@@ -8,30 +9,37 @@ function del(){
     }
 }
 
-function square(){
+function square(randcolor){
     const MAX_SIZE = prompt("How many squares?(less than 100)");
     if(MAX_SIZE>100){
-        square();
+        square(randcolor);
     }
     else{
         del();
-        draw(MAX_SIZE);
+        draw(MAX_SIZE, randcolor);
     }
 }
 
-function draw(MAX_SIZE){
+function draw(MAX_SIZE, randcolor){
     for(let j=0;j<MAX_SIZE;j++){
         const col = document.createElement('div');
         col.style.display = 'flex';
         for(let i=1;i<=MAX_SIZE;i++){
             const di = document.createElement('div');
             let w = 1400/MAX_SIZE;
-            let h = 700/MAX_SIZE;
+            let h = 800/MAX_SIZE;
             di.style.width = w + "px";
             di.style.height = h + "px";
             di.className = "pixel";
             di.addEventListener("mouseenter", ()=>{
-            di.style.backgroundColor = "#FAF9F6";
+                if(randcolor == 1){
+                    di.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255)
+                    + "," + Math.floor(Math.random() * 255) + ","
+                    + Math.floor(Math.random() * 255) + ")";
+                }
+                else{
+                    di.style.backgroundColor = "white";
+                }
     });
 
             col.appendChild(di);
@@ -41,9 +49,16 @@ function draw(MAX_SIZE){
     contain.appendChild(contain1);
 }
 
-const btn = document.querySelector('button');
+const colbtn = document.getElementById('colorbut');
+colbtn.onclick = function(){
+    randcolor = 1;
+    square(randcolor);
+}
+
+const btn = document.getElementById('norbut');
 btn.addEventListener('click', ()=>{
-    square();
+    randcolor = 0;
+    square(randcolor);
 });
 
 const contain = document.querySelector(".container");
